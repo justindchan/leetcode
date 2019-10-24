cb1cc033aabc11bd576466acdfa2de78d491baf6
Justin <justindchan@gmail.com>
2019-10-24 15:16:15 -0700

    implemented solution, O(n) time complexity, odd .sort behavior?

---
+++ heightChecker.js
@@ -0,0 +1,33 @@
+// Students are asked to stand in non-decreasing order of heights for an annual photo.
+
+// Return the minimum number of students not standing in the right positions.  (This is the number of students that must move in order for all students to be standing in non-decreasing order of height.)
+
+// Example 1:
+
+// Input: [1,1,4,2,1,3]
+// Output: 3
+// Explanation: 
+// Students with heights 4, 3 and the last 1 are not standing in the right positions.
+
+// build the correct or of heights by sorting another array, then compare the two arrays. 
+
+
+var heightChecker = function(heights) {
+  // console.log(heights.sort(sortNum));
+  // console.log([...heights].sort(sortNum))
+  let sortedHeights = [...heights].sort(sortNum);
+  // console.log(sortedHeights);
+  let counter = 0;
+  for (let i = 0; i < heights.length; i++) {
+      if (heights[i] !== sortedHeights[i]) {
+          // console.log(heights[i], sortedHeights[i])
+          counter++;
+          // console.log(i)
+      }
+  }
+  return counter;
+};
+
+var sortNum = ((a, b) => a - b)
+
+// for some reason, if the first console.log runs, the code breaks, something to do with .osrt needing spread operator and also not returning an array by default?
