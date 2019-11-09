d98012eb4a84d4d93513132ea6fb49977ff46ec0
Justin <justindchan@gmail.com>
2019-11-08 16:02:33 -0800

    solution implemented, O(n) time, O(1) space

---
+++ reverse-string.js
@@ -0,0 +1,34 @@
+// Write a function that reverses a string. The input string is given as an array of characters char[].
+
+// Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+// You may assume all the characters consist of printable ascii characters.
+
+ 
+
+// Example 1:
+
+// Input: ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+// Example 2:
+
+// Input: ["H","a","n","n","a","h"]
+// Output: ["h","a","n","n","a","H"]
+
+
+// must use pointer variables for temp storage other wise doesn't work.
+var reverseString = function(s) {
+      for (let i = 0, j = s.length-1; i < j; i++, j--) {
+          const x = s[i]; 
+          const y = s[j];
+          s[i] = y; 
+          s[j] = x;
+      }
+  };
+  
+  //  refactored from below
+  //     for (let i = 0, j = s.length - 1; i < j; i++, j--) {
+  //         const x = s[i]; const y = s[j];
+  //         s[i] = s[j];
+  //         s[j] = s[i];
+  //     }    
