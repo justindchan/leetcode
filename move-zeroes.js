923c32a5a1761e6810f71649efc1968a7bfe7450
Justin <justindchan@gmail.com>
2020-01-09 12:05:18 -0800

    refactored solution to O(1) space complexity, O(N) time

--- move-zeroes.js
+++ move-zeroes.js
@@ -14,22 +14,37 @@
 // C: modify arrya in AnimationPlaybackEvent, and minimize total number of operations
 // E: -
 
-// does not fulfill space complexity
 var moveZeroes = function(nums) {
-    let output = [];
-    let zeroCount = 0;
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === 0) {
-            zeroCount++
-        } else {
-            output.push(nums[i])
-        }
-    }
-    for (let j = 0; j < zeroCount; j++) {
-        output.push(0)
-    }
-    return output;
+  let zeroCount = 0;
+  for (let i = 0; i < nums.length; i++) {
+      if (nums[i] === 0) {
+          nums.splice(i, 1)
+          zeroCount++;
+          i--;
+      }
+  }
+  for (let j = 0; j < zeroCount; j++) {
+      nums.push(0)
+  }
+  return nums;
 };
+
+// does not fulfill space complexity
+// var moveZeroes = function(nums) {
+//     let output = [];
+//     let zeroCount = 0;
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] === 0) {
+//             zeroCount++
+//         } else {
+//             output.push(nums[i])
+//         }
+//     }
+//     for (let j = 0; j < zeroCount; j++) {
+//         output.push(0)
+//     }
+//     return output;
+// };
 
 moveZeroes([0,1,0,3,12]) // [1,3,12,0,0]
 moveZeroes([0,0,1]) // [1,0,0]
