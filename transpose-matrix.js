b92169c2bc78eca06f9b72344a2c30cb0b753837
Justin <justindchan@gmail.com>
2020-01-14 11:39:15 -0800

    implemented solution, O(n2) time, O(1) space - in place transposition

---
+++ transpose-matrix.js
@@ -0,0 +1,35 @@
+// Given a matrix A, return the transpose of A.
+
+// The transpose of a matrix is the matrix flipped over it's main diagonal, switching the row and column indices of the matrix.
+
+// Example 1:
+
+// Input:  [[1,2,3],
+//          [4,5,6],
+//          [7,8,9]]
+
+// Output: [[1,4,7],
+//          [2,5,8],
+//          [3,6,9]]
+
+// Example 2:
+
+// Input:  [[1,2,3],
+//          [4,5,6]]
+
+// Output:  [[1,4],
+//           [2,5],
+//           [3,6]]
+ 
+// -- in place transposition for N x N matrix.
+var transpose = function(A) {
+    for (let i = 0; i < A.length; i++) {
+        for (let j = i; j < A.length; j++) {
+            let temp = A[j][i];
+            A[j][i] = A[i][j];
+            A[i][j] = temp;
+        }
+    }
+    return A;
+};
+
