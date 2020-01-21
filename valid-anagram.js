184148ac1709f8e7f4aa6b939ac67135a0918706
Justin <justindchan@gmail.com>
2020-01-20 21:45:33 -0800

    implemented solution, O(n) time, O(n) space

--- valid-anagram.js
+++ valid-anagram.js
@@ -14,3 +14,33 @@
 // Follow up:
 // What if the inputs contain unicode characters? How would you adapt your solution to such case?
 
+var isAnagram = function(s, t) {
+  // use object value pairs, store in temp
+  let isEqual = false;
+  let obj1 = {};
+  let obj2 = {};
+  for (let i = 0; i < s.length;  i++) {
+      if (!obj1[s[i]]) {
+          obj1[s[i]] = 1;
+      } else {
+          obj1[s[i]]++;
+      }
+  }
+  for (let j = 0; j < t.length; j++) {
+      if (!obj2[t[j]]) {
+          obj2[t[j]] = 1;
+      } else {
+          obj2[t[j]]++;
+      }
+  }
+  // console.log(obj1, obj2)
+  // console.log(Object.entries(obj1).sort((a, b) => b[0].localeCompare(a[0])) , Object.entries(obj2).sort((a, b) => b[0].localeCompare(a[0])))
+  obj1 = Object.entries(obj1).sort((a, b) => b[0].localeCompare(a[0]));
+  obj2 = Object.entries(obj2).sort((a, b) => b[0].localeCompare(a[0]));
+  // console.log(JSON.stringify(obj1), JSON.stringify(obj2))
+  if (JSON.stringify(obj1) == JSON.stringify(obj2)) {
+      isEqual = true; 
+  }
+  return isEqual;
+};
+
