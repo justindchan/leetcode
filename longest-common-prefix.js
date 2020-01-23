f0d2762946e7ad74d4d29193beb791c949367c9b
Justin <justindchan@gmail.com>
2020-01-22 17:22:09 -0800

    implementing solution, completed psuedocode, factoring.

--- longest-common-prefix.js
+++ longest-common-prefix.js
@@ -15,3 +15,26 @@
 
 // All given inputs are in lowercase letters a-z.
 
+
+// I: an array of strings
+// O: the longest common prefix among the array
+// C: all given inputs are lowercase letters
+// E: return empty string if no common prefix
+
+var longestCommonPrefix = function(strs) {
+    
+  // get into first array element
+    // get into each element of array
+      // check if each character in first element is found in sequence with the next array element at each character
+        // if not found
+          // return the slice of the first char to the last iteration index, cutting it at the last found common character
+
+  
+  // for (let i = 1; i < strs.length; i++) {
+  //     for (let j = 0; j < strs[i].length; j++) {
+  //         if (current[j] === strs[i][j]) {
+  //             output += current[j]
+  //         }
+  //     }
+  // }
+};
