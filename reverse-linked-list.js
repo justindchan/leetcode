683fd06af509caae74b2e2c067505ba1c5d87ed9
Justin <justindchan@gmail.com>
2020-02-03 23:36:08 -0800

    implementing solution, working through psuedo code

--- reverse-linked-list.js
+++ reverse-linked-list.js
@@ -20,5 +20,13 @@
  * return {ListNode}
  */
 var reverseList = function(head) {
-    
+  // ListNode prev = null;
+  // ListNode curr = head;
+  // while (curr != null) {
+  //     ListNode nextTemp = curr.next;
+  //     curr.next = prev;
+  //     prev = curr;
+  //     curr = nextTemp;
+  // }
+  // return prev;
 };
