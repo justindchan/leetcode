03e46655872d22777c23fe712ca163f053a2881d
Justin <justindchan@gmail.com>
2020-03-05 16:08:32 -0800

    refactored solution to use Set, faster run time, O(n) time, O(n) space, constant time lookup

--- find-the-duplicate-number.js
+++ find-the-duplicate-number.js
@@ -19,17 +19,38 @@
 // I: array of numbers
 // O: the value of the duplcate elements
 // C: only one duplicate number but could be repeated more than once, read-only array, constant space O(1), time coimplexity < O(n2)
-// E: account for no duplicatees?
+// E: account for no duplicatees? return -1
 
+// SOLUTION USING SET AND CONSTANT TIME LOOKUP, O(n) time, O(n) space
+// assign storage for new Set
+// loop through array
+  // if element is present in Set
+    // return that element
+    // else - add element to Set
+
+var findDuplicate = function(nums) {
+  let seen = new Set();
+  for (let i = 0; i < nums.length; i++) {
+    if (seen.has(nums[i])) {
+      return nums[i];
+    } else {
+      seen.add(nums[i]);
+    }
+  }
+  return -1;
+};
+
+
+// SOLUTION USING JS ARRAY METHODS - slower
 // loop through array
   // for each element, 
     // check if index of element is equal to last index of element
       // else, return value
-
-var findDuplicate = function(nums) {
-  for (let i = 0; i < nums.length; i++) {
-    if (nums.indexOf(nums[i]) !== nums.lastIndexOf(nums[i])) {
-      return nums[i];
-    }
-  }
-};
+      
+// var findDuplicate = function(nums) {
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums.indexOf(nums[i]) !== nums.lastIndexOf(nums[i])) {
+//       return nums[i];
+//     }
+//   }
+// };
