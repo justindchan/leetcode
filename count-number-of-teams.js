a2cc85e217c481ce573d8431ccd83d56ed57080d
Justin <justindchan@gmail.com>
2020-04-15 14:39:57 -0700

    cleaned up code

--- count-number-of-teams.js
+++ count-number-of-teams.js
@@ -54,31 +54,32 @@
 //  * @return {number}
 //  */
 const numTeams = (rating) => {
-if(rating.length < 3) return 0;
-let result = 0;
+  if(rating.length < 3) return 0;
+  let result = 0;
 
-let greaterThan = new Array(rating.length).fill(0);
-let lessThan = new Array(rating.length).fill(0);
+  let greaterThan = new Array(rating.length).fill(0);
+  let lessThan = new Array(rating.length).fill(0);
 
-for(let i = 0; i < rating.length; i++) {
-    for(let j = i + 1; j < rating.length; j++) {
-        if(rating[j] > rating[i]) {
-            ++greaterThan[i];
-        } else if(rating[j] < rating[i]) {
-            ++lessThan[i];
-        }
-    }
-}
-    
-for(let i = 0; i < rating.length - 2; i++) {
-    for(let j = i + 1; j < rating.length; j++) {
-        if(rating[j] > rating[i]) {
-            result += greaterThan[j];
-        } else if(rating[j] < rating[i]) {
-            result += lessThan[j];
-        }
-    }
-}
+  for(let i = 0; i < rating.length; i++) {
+      for(let j = i + 1; j < rating.length; j++) {
+          if(rating[j] > rating[i]) {
+              ++greaterThan[i];
+          } else if(rating[j] < rating[i]) {
+              ++lessThan[i];
+          }
+      }
+  }
+      
+  for(let i = 0; i < rating.length - 2; i++) {
+      for(let j = i + 1; j < rating.length; j++) {
+          if(rating[j] > rating[i]) {
+              result += greaterThan[j];
+          } else if(rating[j] < rating[i]) {
+              result += lessThan[j];
+          }
+      }
+  }
 
-return result;    
+  return result;    
 };
+
