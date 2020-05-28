6dc38f2efd23efe6abaa6c0a4fddff147f2ccbf6
Justin <justindchan@gmail.com>
2020-05-28 00:19:19 -0700

    implemented solution, O(n) time, constant space.

--- max-consecutive-ones.js
+++ max-consecutive-ones.js
@@ -30,3 +30,24 @@
         // set max equal to count
   // return max;
 //
+
+var findMaxConsecutiveOnes = function(nums) {
+  let count = 0;
+  let max = 0;
+  if (nums.length === 1) {    
+    return nums[0]
+  } 
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == 0) {
+      count = 0;
+    } else {
+      if (nums[i] == 1) {
+        count++;
+      }
+      if (count > max) {
+        max = count;
+      }
+    }
+  }   
+  return max;
+};
