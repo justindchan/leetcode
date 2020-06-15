cf9d60384f893217b8638f2949e78803f806ff35
Justin <justindchan@gmail.com>
2020-06-15 14:58:55 -0700

    laid out psuedo code

--- valid-mountain-array.js
+++ valid-mountain-array.js
@@ -7,9 +7,6 @@
 // A[0] < A[1] < ... A[i-1] < A[i]
 // A[i] > A[i+1] > ... > A[A.length - 1]
 
-
- 
-
 // Example 1:
 
 // Input: [2,1]
@@ -23,8 +20,27 @@
 // Input: [0,3,2,1]
 // Output: true
  
-
 // Note:
 
 // 0 <= A.length <= 10000
 // 0 <= A[i] <= 10000 
+
+
+// I: given an array, A, of integers
+// O: return true if and only if it is a valid mountain array
+// C: 
+// E: if A.lnegth is 2 or less, if the peak is either the first or last index
+
+// validMountainArray - f(A)
+  // if length of A is less than or equal to 2, return false
+  // if first index of A or last index of A is the peak, return false
+  // store up - boolean flag to toggle if we are ascending in value or descending, determines if we have past the peak yet or not helping ensure we only have one peak
+  // loop through A
+    // if up is true
+      // if current A is less than the previous A, set up flag to false
+      // else if current A is equal to the previous A, return false
+    // else (up is false)
+      // if current A is greater or equal to previous A, return false
+  // return true
+//
+
