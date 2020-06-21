8bdefd6f686540702b9da3f86ed2bd061be9119d
Justin <justindchan@gmail.com>
2020-06-21 13:27:44 -0700

    implemented solution, O(n^2) time, O(1) space

--- replace-elements-with-greatest-element-on-rioght-side.js
+++ replace-elements-with-greatest-element-on-rioght-side.js
@@ -13,3 +13,23 @@
 // 1 <= arr[i] <= 10^5
 
 
+const replaceElements = function(arr) {
+  // loop through arr
+  const n = arr.length;
+  for (let i = 0; i < n; i++) {
+    // if more than 2 elements left, 
+    if (n - i > 2) {
+      // store current maximum to the right 
+      let max = Math.max(...arr.slice(i + 1, n))
+      // replace current with maximum
+      arr[i] = max;
+    } else if ((n-1) - i === 1) {
+      arr[i] = arr[n - 1];
+    } else {
+      arr[i] = -1;
+    }
+  }
+  // return arr  
+  return arr;
+};
+
