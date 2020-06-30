97402296c9e7cd03685344d933038acfe3e89f62
Justin <justindchan@gmail.com>
2020-06-30 13:13:34 -0700

    refactored and optmized original code for faster runtime, added psudocode, cleaned up comments.

--- move-zeroes.js
+++ move-zeroes.js
@@ -11,23 +11,45 @@
 
 // I: an array of nums
 // O: the same array, sorted
-// C: modify arrya in AnimationPlaybackEvent, and minimize total number of operations
+// C: modify arrya in place, and minimize total number of operations
 // E: -
+
+// moveZeroes - f(nums)
+  // initlize zeroCount
+  // loop through nums
+    // if a zero is found
+      // splice it out
+      // increment zeroCount
+      // decrement i to resume from last index
+  // while zeroCount is greater than 0
+    // push a 0 onto the end of nums
+    // decrement zeroCount
+  // return nums
+//
 
 var moveZeroes = function(nums) {
   let zeroCount = 0;
-  for (let i = 0; i < nums.length; i++) {
-      if (nums[i] === 0) {
-          nums.splice(i, 1)
-          zeroCount++;
-          i--;
-      }
-  }
-  for (let j = 0; j < zeroCount; j++) {
-      nums.push(0)
+  for (let i = 0, numsLen = nums.length; i < numsLen; i++) {
+    if (nums[i] === 0) {
+      nums.splice(i, 1);
+      zeroCount++;
+      i--;
+    }
+  }  
+  while (zeroCount > 0) {
+    nums.push(0);
+    zeroCount--;
   }
   return nums;
 };
+
+// made optimizations to original code.
+// Runtime: 76ms, beats 52.11%.
+// Memory Usage: 37.1MB, beats 24.12%.
+
+moveZeroes([0,1,0,3,12]) // [1,3,12,0,0]
+moveZeroes([0,0,1]) // [1,0,0]
+
 
 // does not fulfill space complexity
 // var moveZeroes = function(nums) {
@@ -45,6 +67,3 @@ var moveZeroes = function(nums) {
 //     }
 //     return output;
 // };
-
-moveZeroes([0,1,0,3,12]) // [1,3,12,0,0]
-moveZeroes([0,0,1]) // [1,0,0]
