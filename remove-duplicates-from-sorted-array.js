74f9352a3489bdf787cc3e04c7474689c86779ad
Justin <justindchan@gmail.com>
2020-06-30 12:44:19 -0700

    laid out psuedocode

---
+++ remove-duplicates-from-sorted-array.js
@@ -0,0 +1,25 @@
+// Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+
+// Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+// Example 1:
+
+// Given nums = [1,1,2],
+
+// Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+
+// It doesn't matter what you leave beyond the returned length.
+
+
+// I: a sorted array of numbers.
+// O: redturn the original array, containing no duplicate elements. 
+// C: 
+// E:
+
+// removeDuplcates - f(nums)
+  // initilize i
+  // while current is equal to pointer, remove pointer
+    // else, increment i
+  // return nums
+// 
+
