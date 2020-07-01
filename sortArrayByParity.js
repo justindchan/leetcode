5bef0a7179f30d0a43d39c9e70dd4474de30d2b1
Justin <justindchan@gmail.com>
2020-07-01 12:20:38 -0700

    added psuedo code and runtime stats

--- sortArrayByParity.js
+++ sortArrayByParity.js
@@ -1,10 +1,23 @@
 // Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
 // You may return any answer array that satisfies this condition.
 
+
 // I: an unsorted array: A
 // O: a sorted array consisting of all the even elements of A followed by all the odd elements of A.
 // C:  
 // E: 
+
+// sortArrayByParity - f(A)
+  // store evens output array
+  // store odd output array
+  // forEach element in A
+    // if element modulo 2 is equal to 0
+      // push element to evens array
+    // else 
+      // push element to odds array
+  //return evens concatenated to odds
+//
+
 var sortArrayByParity = function(A) {
   let evens = [];
   let odds = [];
@@ -13,3 +26,6 @@ var sortArrayByParity = function(A) {
   })
   return evens.concat(odds);
 };
+
+// Runtime: 80ms, beats 83.22%
+// Memory: 38.4MB 37.75%
