0860b981dd6961a5c927bb52cd1d41752f390157
Justin <justindchan@gmail.com>
2020-07-07 13:27:28 -0700

    implemented solution with sort, O(n) time

--- third-maximum-number.js
+++ third-maximum-number.js
@@ -20,16 +20,36 @@
 // Both numbers with value 2 are both considered as second maximum.
 
 
-// i: given a non-empty sarray of integers
-// o: return the third maximum number in the array, if this does not exist, return the max number
-// c: time complexity must be O(n), 
-// e: input array has length 0, 1, or 2
+// i: given a non-empty sarray of integers.
+// o: return the third maximum number in the array, if this does not exist, return the max number.
+// c: time complexity must be O(n).
+// e: input array has length 0, 1, or 2.
 
 // thirdMax - f(nums)
-  // account for edge cases
-    // if nums.length is less than 3
-      // return the max value found in nums 
-  // turn nums into a SET
-  // ensure the SET is sorted or the array was pre-sorted
-  // return the third to last index (third max value) as SET[SET.length -3]
+// turn nums into a SET to create an array with only distnct elements.
+  // account for edge cases.
+    // if nums.length is less than 3.
+      // return the max value found in nums.
+  // ensure the SET is sorted or the array was pre-sorted.
+  // return the third to last index (third max value) as SET[SET.length -3].
 // 
+
+
+const sortNum = ((a, b) => a - b)
+
+const thirdMax = function(nums) {
+  // filter non distinct values and sort
+  let distinctArr = Array.from(new Set(nums)).sort(sortNums);
+  let len = distinctArr.length 
+  // handle edge case
+  if (len < 3) {
+    return Math.max(...distinctArr);
+  }
+  // find third max
+  return distinctArr[len - 3]
+};
+
+let nums = [3,2,1];
+thirdMax(nums); // 1
+
+ 
