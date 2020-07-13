8f0bfc442246c401bb17232de99b036f06d2e5b4
Justin <justindchan@gmail.com>
2020-07-13 15:01:18 -0700

    laid out psuedo code, solution is not O(n) time

--- find-all-numbers-disappeared-in-an-array.js
+++ find-all-numbers-disappeared-in-an-array.js
@@ -11,3 +11,19 @@
 
 // Output:
 // [5,6]
+
+
+// i: an array of integers, where each element is equal to or greater than 1 and less than or equal to n (n equals the size of the array).
+// o: all elements of [1,n] inclusive that do not appear in this array.
+// c: O(1) space, O(n) time, returned list does not count as extra space. 
+// e: array length 1?
+
+// findDisappeared - f(nums)
+  // store output array - []
+  // store max value - max = nums.length
+  // loop 1 -> max
+    // if current i is not included in nums, 
+      // push current i to output
+  // return output 
+//
+
