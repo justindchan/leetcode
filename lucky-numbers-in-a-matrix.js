4d9a7673bc38f1943bd8e91175fd9df4632fa7cd
Justin <justindchan@gmail.com>
2020-09-05 21:53:38 -0700

    implemented solution

--- lucky-numbers-in-a-matrix.js
+++ lucky-numbers-in-a-matrix.js
@@ -48,3 +48,25 @@
   // return output;
 //
 
+var luckyNumbers  = function(matrix) {
+  let happyArr = [];
+  const cLen = matrix.length;
+  const rLen = matrix[0].length;
+  
+  for (let i = 0; i < cLen; i++) {
+    outer: 
+    for (let j = 0; j < rLen; j++) {
+      const currEl = matrix[i][j];
+      
+      for (let c = 0; c < cLen; c++) {
+        if (c !== i && currEl < matrix[c][j]) continue outer;
+      }
+      for (let r = 0; r < rLen; r++) {
+        if (r !== j && currEl > matrix[i][r]) continue outer;
+      }
+      
+      happyArr.push(currEl);
+    }
+  }
+  return happyArr;
+};
