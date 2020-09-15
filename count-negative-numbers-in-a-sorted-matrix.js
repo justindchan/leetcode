b9c1f6d0920ca702925ad6754800bf5f3828343b
Justin <justindchan@gmail.com>
2020-09-15 13:31:48 -0700

    implemented solution O(n^2) time, O(1) space

--- count-negative-numbers-in-a-sorted-matrix.js
+++ count-negative-numbers-in-a-sorted-matrix.js
@@ -31,4 +31,14 @@
 // -100 <= grid[i][j] <= 100
 
 
-
+var countNegatives = function(grid) {
+  let negatives = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] < 0) {
+        negatives++;
+      }
+    }
+  }
+  return negatives;
+};
